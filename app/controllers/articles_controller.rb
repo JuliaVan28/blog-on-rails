20a5f338e6f8ad10055bda79277774f81e52ac45
JuliaVan28 <julia.van2801@gmail.com>
2021-05-24 16:39:40 +0300

    Funcion of adding new posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,25 @@ class ArticlesController < ApplicationController
   end
 
   def show 
-    @posts = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new.html.erb
+    end
+  end
+
+  private
+    def post_params
+      params.require(:articles).permit(:title, :summary, :body)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :summary, presence: true, length: { maximum: 15 }
+  validates :body, presence: true, length: { minimum: 5 }
 end
